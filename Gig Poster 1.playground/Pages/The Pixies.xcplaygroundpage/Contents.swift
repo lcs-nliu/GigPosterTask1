6b82121bff25872592ef51b5f84313f59ca962d8
Nicole Liu <nliu@lcs.on.ca>
2020-01-19 14:21:58 -0500

    Finished the background and dots

--- Gig Poster 1.playground/Pages/The Pixies.xcplaygroundpage/Contents.swift
+++ Gig Poster 1.playground/Pages/The Pixies.xcplaygroundpage/Contents.swift
@@ -23,10 +23,39 @@ import PlaygroundSupport
 
 // Create a new canvas
 let canvas = Canvas(width: 400, height: 600)
+// colour the background in lime green
+canvas.fillColor = Color(hue: 106, saturation: 64, brightness: 73, alpha: 100)
+canvas.drawRectangle(at: Point(x: 0, y:0), width: 400, height: 600)
+// draw black rectangle
+canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 100)
+canvas.drawRectangle(at: Point(x: 0, y:0), width: 400, height: 400)
+// no borders
+canvas.drawShapesWithBorders = false
 
-// COLORS
-let offWhite = Color(hue: 81, saturation: 5, brightness: 88, alpha: 100)
-let limeGreen = Color(hue: 106, saturation: 64, brightness: 73, alpha: 100)
+// draw the dots on the vertical
+for height in stride (from: 0, through: 400, by: 40){
+    // draw the dots on the horizontal
+    for position in stride (from: 0, through: 400, by: 40){
+        // find placement of dot to determine colour
+        let place = height + position
+        print(place)
+        if position == 0 || height == 0 {
+           canvas.fillColor = Color(hue: 106, saturation: 64, brightness: 73, alpha: 100)
+        } else if place >= 40 && place <= 400{
+         canvas.fillColor = Color(hue: 81, saturation: 5, brightness: 88, alpha: 100)
+        } else {
+        canvas.fillColor = Color(hue: 106, saturation: 64, brightness: 73, alpha: 100)
+        }
+       // draw the circle
+        canvas.drawEllipse(at: Point(x: position, y: height), width: 35, height: 35)
+    }
+}
+
+
+//
+//// COLORS
+//let offWhite = Color(hue: 81, saturation: 5, brightness: 88, alpha: 100)
+//let limeGreen = Color(hue: 106, saturation: 64, brightness: 73, alpha: 100)
 
 // Begin your solution here...
 
@@ -35,7 +64,7 @@ let limeGreen = Color(hue: 106, saturation: 64, brightness: 73, alpha: 100)
  ## Use Source Control
  
  Remember to commit and push your work before 11:30 AM on Monday, January 13, 2020, please.
-
+ 
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
  */
